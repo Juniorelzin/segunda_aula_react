4b9999aeaa3b880295f27c294898d8871bfc5be3
ESTEVAO LIMAS JUNIOR <estevao_limas-jun@estudante.sesisenai.org.br>
2024-08-13 22:03:28 -0300

    Em processo

---
+++ Farmacia/src/components/conteudo/index.jsx
@@ -0,0 +1,31 @@
+import './index.css'
+function Conteudo() {
+  return (
+    <div className="conteudo-container">
+      
+      
+        <img className="conteudo-logo" src="./public/images/farmarcia.png" alt="" />
+
+
+
+    <div className='conteudo-texto'>
+        <p>
+          Estamos entusiasmados em anunciar uma nova fase para a nossa farmácia! Com a chegada de uma nova direção
+        </p>
+
+        <ul>
+          <li>Atendimento Personalizado: Nossa equipe está treinada para entender e atender às suas necessidades de saúde com atenção e cuidado.</li>
+          <li>Ampla Variedade de Produtos: Desde medicamentos até produtos de cuidados pessoais e suplementos, você encontrará tudo o que precisa em um só lugar.</li>
+          <li>Serviços Inovadores: Oferecemos serviços de saúde como aferição de pressão arterial, acompanhamento de tratamentos e muito mais.</li>
+        </ul>
+    </div>
+
+        
+        
+    
+
+    </div>
+  )
+}
+
+export default Conteudo
---
+++ Farmacia/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Conteudo from "../components/conteudo"
+import Header from "../components/Header"
+
+function Home() {
+  return (
+    <div>
+        <Header />
+        <Conteudo />
+    </div>
+  )
+}
+
+export default Home
